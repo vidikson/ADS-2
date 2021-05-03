76154feea62576d5315d83a25e72777eb273abc6
vidikson <65093952+vidikson@users.noreply.github.com>
2021-05-03 11:20:50 +0300

    Кутузова Виктория

--- src/alg.cpp
+++ src/alg.cpp
@@ -13,7 +13,7 @@ int countPairs1(int *arr, int len, int value) {
 }
 
 int countPairs2(int *arr, int len, int value) {
- int count = 0, num = len - 1;
+   int count = 0, num = len - 1;
     while ((arr[0] + arr[num]) > value) {
         num--;
     }
@@ -28,23 +28,23 @@ int countPairs2(int *arr, int len, int value) {
 }
 
 int countPairs3(int *arr, int len, int value) {
- int res = 0, l = 0, r = len - 1;
-    while ((r - 1) > l) {
-      int mid = (r + l) / 2;
-        if (value >= arr[mid]) 
-        l = mid;
+   int res = 0, l = 0, r = len - 1;
+      while ((r - 1) > l) {
+         int mid = (r + l) / 2;
+            if (value >= arr[mid])
+               l = mid;
             else r = mid;
-    }       
+      }
     len = r - 1;
         for (int i = 0; i < len; i++) {
             l = i + 1;
             r = len - 1;
                 int res2 = 0;
                 while (r > l) {
-                    int mid = (r + l) / 2;
-                        if ((value - arr[i]) > arr[mid]) 
-                            l = mid + 1;
-                        else r = mid;
+                   int mid = (r + l) / 2;
+                     if ((value - arr[i]) > arr[mid])
+                        l = mid + 1;
+                     else r = mid;
                 }
                 while ((value - arr[i]) == arr[l]) {
                     res2++;
